77d6fb30c4dded93b225c4cec1326d98e54988e1
lh <jyfc7879@gmail.com>
2016-06-20 20:25:33 +0800

    servlet <servlet-name> <url-pattern> 匹配规则 the container prefers an exact path match over a wildcard path match. Second, the container prefers to match the longest pattern. Third, the container prefers path matches over filetype matches. Finally, the pattern <url-pattern>/</url-pattern> always matches any request that no other pattern matches

--- src/main/java/com/crunchify/controller/CrunchifyHelloWorld.java
+++ src/main/java/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class CrunchifyHelloWorld {
-        @RequestMapping("/welcome")
+        @RequestMapping("/welcome/mm")
     public ModelAndView helloWorld() {
         String message = "<br><div style='text-align:center;'>"
                 + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
